c5068edfe4e8161a6ddd788409b8bf05081264f0
Kumenthula Shivani <158257138+Kumenthula-Shivani08@users.noreply.github.com>
2024-07-05 16:40:35 +0530

    Add files via upload

---
+++ even.js
@@ -0,0 +1,17 @@
+
+
+<html>
+<head>
+	<title>Even or Odd Number</title>
+</head>
+<body>
+	<script>
+		const number = prompt("Enter a number: ");
+		if (number % 2 == 0) {
+			console.log("The number is even");
+		} else {
+			console.log("The number is odd");
+		}
+	</script>
+</body>
+</html>
